3e38edb58d8ab420aeaf6ed15d60b63d08c7eb40
DaneMaldonado <dmaldonado2025@communityschoolnaples.org>
2024-08-16 11:29:20 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let mySprite = sprites.create(img`
+    . . . . . . . b b . . . . . . . 
+    . . . . . . b d d b . . . . . . 
+    . . . . . b d 5 5 d b . . . . . 
+    . . . . b b 5 5 5 5 b b . . . . 
+    . . . . b 5 5 5 5 5 5 b . . . . 
+    b b b b b 5 5 5 5 1 1 d b b b b 
+    b 5 5 5 5 5 5 5 5 1 1 1 5 5 5 b 
+    b d d 5 5 5 5 5 5 1 1 1 5 d d b 
+    . b d d 5 5 5 5 5 5 5 5 d d b . 
+    . . b b 5 5 5 5 5 5 5 5 b b . . 
+    . . c b 5 5 5 5 5 5 5 5 b c . . 
+    . . c 5 5 5 5 d d 5 5 5 5 c . . 
+    . . c 5 5 d b b b b d 5 5 c . . 
+    . . c 5 d b c c c c b d 5 c . . 
+    . . c c c c . . . . c c c c . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food)
